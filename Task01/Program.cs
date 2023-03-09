dd5a7083ad6e7a367cc10cf5127a9e3aa3c0f6e0
Vera <v.khotsko@gmail.com>
2023-03-09 22:48:31 +0300

    Added program about square and two numbers

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пр-ма, которая на вход принимает 2 числа и
+// проверяет, является ли первое квадратом второго.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите первое целое число: "); // взаимодействие с пользователем
+        int number1 = Convert.ToInt32(Console.ReadLine()); // перевод строки в целочисленный тип
+
+        Console.Write("Введите второе целое число: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        int square = number2 * number2; // дополнительная переменная для действия квадрата
+        if (number1 == square) // условие ветвление
+        {
+            Console.WriteLine("Первое число ЯВЛЯЕТСЯ квадратом второго");
+        }
+        else
+        {
+            Console.WriteLine("Первое число НЕ ЯВЛЯЕТСЯ квадратом второго");
+        }
+    }
+}
