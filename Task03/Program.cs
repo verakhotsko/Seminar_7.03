d1f9777e1ae35f59186766f59b1104d4d940a5f8
Vera <v.khotsko@gmail.com>
2023-03-09 23:21:27 +0300

    Added program about number and day of the week

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пр-ма, которая будеn выдавать день недели по заданному номеру.
+
+Console.Write("Введите номер от 1 до 7, включительно: ");
+string number = Console.ReadLine();
+
+if (number == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+if (number == "2")
+{
+    Console.WriteLine("Вторник");
+}
+if (number == "3")
+{
+    Console.WriteLine("Среда");
+}
+if (number == "4")
+{
+    Console.WriteLine("Четверг");
+}
+if (number == "5")
+{
+    Console.WriteLine("Пятница");
+}
+if (number == "6")
+{
+    Console.WriteLine("Суббота");
+}
+if (number == "7")
+{
+    Console.WriteLine("Воскресение");
+}
