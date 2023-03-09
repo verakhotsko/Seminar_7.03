ab49633e58fc3d61b86e530318044111d9643330
Vera <v.khotsko@gmail.com>
2023-03-09 17:50:16 +0300

    Программа про квадрат числа

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// 1. на вход принимает число и 
+// 2. выдает его квадрат.
+
+// string input = Console.ReadLine(); // присваиваем строке нужную команду
+// int number = Convert.ToInt32(input); // конвертируем строку в целочисленный тип
+Console.Write("Введите целое число: "); // взаимодействие с пользователем
+int number = Convert.ToInt32(Console.ReadLine()); // в одну строку
+int square = number * number; // само действие
+Console.WriteLine($"Квадрат числа {number} = {square}"); //интерполяция строки в действия
