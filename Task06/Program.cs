7f50e3c16213b4fe2bca53c50cd3004aca56a206
Vera <v.khotsko@gmail.com>
2023-03-10 22:31:56 +0300

    Added program about even number

--- Task06/Program.cs
+++ Task06/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пр-ма, котрая на вход принимает число, а 
+// на выход выдает - является ли число четным.
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Является чётным");
+}
+else
+{
+    Console.WriteLine("Не является чётным");
+}
