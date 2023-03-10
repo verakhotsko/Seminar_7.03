38ccedb4b9874ef669ffd6e83577ed293d54c0a4
Vera <v.khotsko@gmail.com>
2023-03-10 22:17:22 +0300

    Added second variant to the Task04'

---
+++ Task04.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пр-ма, принмает на вход 3 числа, а
+// на выход выдает максимальное из этих чисел.
+
+Console.Write("Введите первое целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье целое число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 > number1)
+{
+    number1 = number2;
+}
+if (number3 > number1)
+{
+    number1 = number3;
+}
+Console.WriteLine($"Максимальное число: {number1}");
