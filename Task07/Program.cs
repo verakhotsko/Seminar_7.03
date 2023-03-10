cbcf0d53f748d5d6193838a9b6799ba809c186d8
Vera <v.khotsko@gmail.com>
2023-03-10 11:05:18 +0300

    Added program about the last digit of a three-digit number

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пр-ма, которая принимает на вход трёхзначное число,
+// а на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999) // вводим условие для точного введения трёхзначного числа
+{
+    int lastNumber = number % 10;
+    Console.WriteLine(lastNumber);
+}
+else
+{
+    Console.WriteLine("Трёхзначное число это три цифры подряд. Ввведите правильное число.");
+}
