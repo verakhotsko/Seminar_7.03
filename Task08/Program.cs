3f210aff875c59cca9f06e0a4b71224c6de41f78
Vera <v.khotsko@gmail.com>
2023-03-10 22:56:21 +0300

    Added program about range of the even numbers

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пр-ма, которая на вход принимает число (N), а
+// на выходе показывает все четные числа от 1 до N.
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= number)
+{
+    Console.Write($"{count}, ");
+    count +=2;
+}
+
