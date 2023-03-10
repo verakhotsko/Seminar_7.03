902d717b9a8bbd26b83753aa5d759af7ec1e6801
Vera <v.khotsko@gmail.com>
2023-03-10 22:15:40 +0300

    Added programm about max of the three numbers

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пр-ма, принмает на вход 3 числа, а
+// на выход выдает максимальное из этих чисел.
+
+Console.Write("Введите первое целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье целое число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+
+if (number2 > max)
+{
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+Console.WriteLine($"Максимальное число: {max}");
