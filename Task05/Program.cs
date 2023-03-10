8bf7186610f5492291173e312bd9974a5fed873c
Vera <v.khotsko@gmail.com>
2023-03-10 10:21:24 +0300

    Added program about N -> -N..N

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пр-ма, которая на входе принимает целое положительное число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number; // счётчик
+while(count <= number)
+{
+    Console.Write($"{count}, "); // начнёт с отрицательного числа
+    count++;
+}
