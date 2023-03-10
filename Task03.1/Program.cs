26997807209d2322ae5721035abf903dc69aaec5
Vera <v.khotsko@gmail.com>
2023-03-10 11:22:11 +0300

    Added program about days of the week using if else

--- Task03.1/Program.cs
+++ Task03.1/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Программа, которая будет выдавать название дня недели
+//по заданному номеру.
+//Реализация через "if else".
+
+Console.WriteLine("Введите номер дня недели: ");
+string day = Console.ReadLine();
+if (day == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+else if (day == "2")
+{
+    Console.WriteLine("Вторник");
+}
+else if (day == "3")
+{
+    Console.WriteLine("Среда");
+}
+else if (day == "4")
+{
+    Console.WriteLine("Четверг");
+}
+else if (day == "5")
+{
+    Console.WriteLine("Пятница");
+}
+else if (day == "6")
+{
+    Console.WriteLine("Суббота");
+}
+else if (day == "7")
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Дня недели с таким номером не существует.");
+}
+
+
+
+
+
