181b1dd8e388db1b6d069358f4e35e39c98c1303
Vera <v.khotsko@gmail.com>
2023-03-14 13:06:50 +0300

    Added programm about removing second digit from three-digit number

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пр-ма, которая выводит случайное трёхзначное число
+// и удаляет вторую цифру этого числа.
+
+
+int DelSecDig(int num)
+{
+    int numFirst = num / 100;
+    int numLast = num % 10;
+    int delSecDig = numFirst * 10 + numLast;
+    return delSecDig;
+}
+
+int number = new Random().Next(100, 999);
+
+Console.WriteLine($"Случайное трёхзначное число: {number}.");
+Console.WriteLine($"Двухзначное число: {DelSecDig(number)}.");
