babb3d4ff5628485365a448b04de284624a645b1
Vera <v.khotsko@gmail.com>
2023-03-14 20:21:58 +0300

    Added program about Remainder

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пр-ма, которая на вход принимает два числа и 
+// выводит, является ли первое число кратное второму.
+// Если не является, то выводит остаток от деления.
+
+
+// Метод:
+int Remainder(int num1, int num2)
+{
+ int remNum = num1 % num2;
+ return remNum;
+}
+// Программа:
+Console.WriteLine("Введите первое целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (Remainder(number1, number2) == 0) Console.WriteLine($"Первое число КРАТНО второму.");
+else Console.WriteLine($"Не кратно. Отстаток от деления: {Remainder(number1, number2)}");
