39ddfacadc60fb7d2632335a0d1dd44b4fa33471
Vera <v.khotsko@gmail.com>
2023-03-15 19:58:01 +0300

    Added program about second digital

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пр- ма, которая на вход принимает трёхзначное число и 
+// на выход показывает вторую цифру этого числа.
+
+//Метод
+int SecondDig(int num)
+{
+    int num1 = num % 100;
+    int num2 = num1 / 10;
+    return num2;
+}
+// Программа
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вторая цифра этого числа: {SecondDig(number)}");
