600f0f058f49e149ab6fac4af2f44156abb84eef
Vera <v.khotsko@gmail.com>
2023-03-15 16:22:38 +0300

    Added program about square of two numbers

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пр-ма, которая на вход принимает два числа, и
+// проверяет является ли одно квадратом другого.
+
+bool IsEqualSquared(int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1; 
+}
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsEqualSquared(number1, number2);
+Console.WriteLine(result ? "Является" : "Не является");
+
+
