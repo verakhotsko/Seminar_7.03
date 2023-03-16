ec36fb5c0b1ecb5dc76d65fbd3540a9e7f565420
Vera <v.khotsko@gmail.com>
2023-03-16 10:36:24 +0300

    Добавила решение от ГБ

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -12,3 +12,22 @@ int SecondDig(int num)
 Console.WriteLine("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Вторая цифра этого числа: {SecondDig(number)}");
+
+// Решение от GeekBrains:
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int number = Prompt("Введите трёхзначное число");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число. Введите другое.");
+    return;
+}
+Console.WriteLine($"Введенное число {number}");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра {secondRank}");
