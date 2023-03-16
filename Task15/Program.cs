291bc3b428fc19c0301d13571baf9f8dd02231d9
Vera <v.khotsko@gmail.com>
2023-03-16 10:22:53 +0300

    Added Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пр-ма, которая на вход принимает цифру, обозначающую день недели
+// и на выход проверяет, является ли эта цифра выходным.
+
+Console.WriteLine("Введите цифру от 1 до 7, включительно: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 5)Console.WriteLine("да, выходной");
+else Console.WriteLine("нет, рабочий");
+
