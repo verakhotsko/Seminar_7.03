05b8547b80eb9e93a23bb5e585241738ba2d46d1
Vera <v.khotsko@gmail.com>
2023-03-16 10:38:19 +0300

    Изменила комментарии в Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -2,16 +2,16 @@
 // на выход показывает вторую цифру этого числа.
 
 //Метод
-int SecondDig(int num)
-{
-    int num1 = num % 100;
-    int num2 = num1 / 10;
-    return num2;
-}
+//int SecondDig(int num)
+//{
+//    int num1 = num % 100;
+//    int num2 = num1 / 10;
+//    return num2;
+//}
 // Программа
-Console.WriteLine("Введите трёхзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Вторая цифра этого числа: {SecondDig(number)}");
+//Console.WriteLine("Введите трёхзначное число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"Вторая цифра этого числа: {SecondDig(number)}");
 
 // Решение от GeekBrains:
 int Prompt(string message)
