ab61da8fc0a549d5d66179ad6ad90dcd99574bd1
Vera <v.khotsko@gmail.com>
2023-03-16 13:36:09 +0300

    Программа о расстоянии между двумя точками на графике

---
+++ Task20/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пр-ма, которая на вход принимает координаты двух точек и 
+// на выход выводит расстояние между ними в 2Д пространстве.
+
+double Distance(int X1, int Y1, int X2, int Y2)
+{
+   int distX1X2 = X1 - X2;
+   int distY1Y2 = Y1 - Y2;
+   int sumSquare = distX1X2 * distX1X2 + distY1Y2 * distY1Y2;
+   double result = Math.Sqrt(sumSquare);
+   double resultRound = Math.Round(result, 2, MidpointRounding.ToZero);
+   return resultRound;
+}
+
+
+Console.Write("Введите координаты ПЕРВОЙ точки. ");
+Console.WriteLine("Координата Х: ");
+int coordX1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Координата Y: ");
+int coordY1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты ВТОРОЙ точки. ");
+Console.WriteLine("Координата Х: ");
+int coordX2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Координата Y: ");
+int coordY2 = Convert.ToInt32(Console.ReadLine());
+
+double distanse = Distance(coordX1, coordY1, coordX2, coordY2);
+Console.WriteLine(distanse);
