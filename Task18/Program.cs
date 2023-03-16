5cfad165871be08dd544970e71f08eb74f63acd9
Vera <v.khotsko@gmail.com>
2023-03-16 12:19:42 +0300

    Программа соответствия диапазона координат заданной четверти

---
+++ Task18/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пр-ма, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (х и у).
+
+string Coordinate(string quater) // string X, string Y
+{
+    if (quater == "1") return "X > 0, Y > 0";
+    if (quater == "2") return "X < 0, Y > 0";
+    if (quater == "3") return "X < 0, Y < 0";
+    if (quater == "4") return "X > 0, Y < 0";
+    return "Такой четверти нет.";
+}
+
+Console.Write("Введите номер четверти ");
+string number = Console.ReadLine();
+string coordinate = Coordinate(number);
+Console.WriteLine(coordinate);
