f5b0b929410708e2b47ba1d6e486e66f8d82988e
Vera <v.khotsko@gmail.com>
2023-03-17 11:54:51 +0300

    Программа о таблице кубов от чисел в заданном диапазоне

---
+++ Task23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пр-ма, которая на вход принимает число (N), а 
+// на выход выдаёт таблицу кубов чисел от 1 до N.
+
+void TabCube(int num)
+{
+int count = 0;
+
+    while (count <= num)
+    {
+       int cube = count * count * count;
+       Console.WriteLine($"{count, 3}  ->  {cube, 5}"); 
+       count ++;
+    }
+}
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+TabCube(number);               
