c1432fede087c19fa5d1c3398c86c565dfa90436
Vera <v.khotsko@gmail.com>
2023-03-17 10:30:40 +0300

    Программа о таблице квадратов чисел в заданном диапазоне

---
+++ Task22/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пр-ма, которая на вход принимает число (N),
+// а на выход выдает таблицу квадратов чисел.
+
+void TabSquare(int num)
+{
+int count = 0;
+
+    while (count <= num)
+    {
+       int square = count * count;
+       Console.WriteLine($"{count, 3}  ->  {square, 5}"); // выравнивание по длине строки
+       count ++;
+    }
+}
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+TabSquare(number);                  // так вызывается метод Void
