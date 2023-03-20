3fb8f11cb99f346df003864fd32c47f70deb627b
Vera <v.khotsko@gmail.com>
2023-03-20 15:06:23 +0300

    Программа нахождения произведения чисел от 1 до заданнго

---
+++ Task28/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая на вход принимает число N, а 
+// на выход выдаёт произведение чисел от 1 до N.
+
+int Factorial(int num)
+{
+    int fact = 1;
+    for(int i = 2; i <= num; i ++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int factorial = Factorial(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} равно {factorial}.");
