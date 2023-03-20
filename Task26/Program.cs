bc4f6bc1430617285e592e0cbbe67f1f6bc655f7
Vera <v.khotsko@gmail.com>
2023-03-20 14:34:57 +0300

    Программа на количество цифр в числе

---
+++ Task26/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пр-ма, которая принимает на вход число и
+// выдает количество цифр в числе.
+
+int CountDigit(int num)
+{
+    int count = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        count ++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int countDigit = CountDigit(number);
+Console.WriteLine($"Количество цифр в числе {number}: {countDigit}.");
