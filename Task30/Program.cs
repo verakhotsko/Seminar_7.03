a26c398dfa8b0ccabbd98fb7b97ea2d0c2408a9e
Vera <v.khotsko@gmail.com>
2023-03-20 16:21:15 +0300

    Программа по заполнению массива и выведению его

---
+++ Task30/Program.cs
@@ -0,0 +1,25 @@
+﻿// Пр-ма, которая выводит массив из 8 элементов,
+// заполненный нулями и единицами в случайном порядке.
+
+
+void FillArray(int[]num)
+{
+    int length = num.Length;
+    for(int i = 0; i < length; i ++)
+    {
+        num[i] = new Random().Next(0, 2);
+    }
+}
+
+void PrintArray(int[]numb)
+{
+    int count = numb.Length;
+    for(int j = 0; j < count; j ++)
+    {
+        Console.Write(numb[j] + " ");
+    }
+}
+
+int []array = new int [8];
+FillArray(array);
+PrintArray(array);
