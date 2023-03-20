91d12c2f4592240fc8162aeb96e68dc50914a541
Vera <v.khotsko@gmail.com>
2023-03-20 15:14:02 +0300

    Правка на переполнение типа

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -6,7 +6,10 @@ int Factorial(int num)
     int fact = 1;
     for(int i = 2; i <= num; i ++)
     {
-        fact = fact * i;
+        checked                    // на переполнение типа int
+        {
+            fact = fact * i;
+        }
     }
     return fact;
 }
