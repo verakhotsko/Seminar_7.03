f8c2e723ac415e3585b47d2e1a33cfdc6b5b8ac7
Vera <v.khotsko@gmail.com>
2023-03-20 13:52:09 +0300

    Программа нахождения суммы чисел от 1 до заданного

---
+++ Task24/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пр-ма, которая на вход принимает число (А), а 
+// на выход выдаёт сумму чисел от 1 до А.
+
+int Sumnumbers(int num)
+{
+    int sum = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        sum = sum + i; // sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumNubers = Sumnumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} равна: {sumNubers}");
