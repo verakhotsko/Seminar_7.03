50ba69d19ca6d844b1f50ae27fe6d15ba2daaa49
Vera <v.khotsko@gmail.com>
2023-03-21 12:39:30 +0300

    Программа для нахождения суммы всех цифр в заданном числе

---
+++ Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пр-ма, которая принимает на вход число и 
+// выдвёт сумму цифр в числе.
+
+// Метод на подсчёт суммы всех цифр в числе:
+
+int SumDigital(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма всех цифр в числе {number} равна {SumDigital(number)}.");
