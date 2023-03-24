9353ed34596926e8f4f2047cb4b78a19748f80a7
Vera <v.khotsko@gmail.com>
2023-03-24 16:22:26 +0300

    Программа разворачивающая массив

---
+++ Task39/Program.cs
@@ -0,0 +1,42 @@
+﻿// Пр-ма, которая перевернёт одномерный массив.
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+// метод, выводящий массив:
+
+void PrintArray(int[]arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+// метод, разворачивающий массив
+void ReverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp; 
+    }
+}    
+
+int[] array = CreateArray(8, 1, 9);
+PrintArray(array);
+ReverseArray(array);
+Console.WriteLine();
+PrintArray(array);
