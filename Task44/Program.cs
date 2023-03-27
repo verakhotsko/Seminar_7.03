508a03d3b21688a006971d3ccd0a1fb9e9b917e2
Vera <v.khotsko@gmail.com>
2023-03-27 18:16:03 +0300

    Программа, создающая массив Фибоначчи до N

---
+++ Task44/Program.cs
@@ -0,0 +1,30 @@
+﻿// Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// если N = 5 -> 01123
+
+int[] FibonachiArr(int num)
+{
+   int[] arr = new int[num];
+   arr[0] = 0;
+   arr[1] = 1;
+   for (int i = 2; i < num; i++)
+   {
+      arr[i] = arr[i - 2] + arr[i - 1];
+   } 
+  return arr;
+}
+
+void PrintArray(int[]arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = FibonachiArr(number);
+PrintArray(array);
