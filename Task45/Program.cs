a26cc57c0126ae934c1dc4aad73a70a86b7eaa4e
Vera <v.khotsko@gmail.com>
2023-03-27 18:35:53 +0300

    Программа, копирующая исходный массив поэлементно

---
+++ Task45/Program.cs
@@ -0,0 +1,40 @@
+﻿// Пр-ма, кщторая создаёт копию заданного массива
+// с помощью поэлементного копирования.
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[]arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] CopyArray(int[]arr)
+{
+    int[] copyArr = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        copyArr[i] = arr[i];
+    }
+    return copyArr;
+}
+
+int[] array = CreateArray(10, 2, 12);
+PrintArray(array);
+Console.WriteLine();
+int[] copyArray = CopyArray(array);
+PrintArray(copyArray);
