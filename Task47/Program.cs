2667b6e37a9dc324c898e9e034bba86e0d6d0935
Vera <v.khotsko@gmail.com>
2023-03-31 11:26:09 +0300

    Задать двумерный массив вещественных чисел

---
+++ Task47/Program.cs
@@ -0,0 +1,33 @@
+﻿//  Задайте двумерный массив размером м*н.
+// Заполните его вещественными числами.
+
+double[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) //для строк
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // для столбцов
+        {
+            matrix[i, j] = rnd.NextDouble() * (max - min) + min;
+            matrix[i, j] = Math.Round(matrix[i, j], 1, MidpointRounding.ToZero);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j], 7} ");
+    }
+    Console.WriteLine("|");
+    }
+}
+
+double[,] array2d = CreateMatrix(3, 7, -10,10);
+PrintMatrix(array2d);
