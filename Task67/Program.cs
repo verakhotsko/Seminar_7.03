4868bd98dd886c18ad4a6577eee7b8f8626e7766
Vera <v.khotsko@gmail.com>
2023-04-11 12:12:22 +0300

    Рекурсия по сумме цифр числа.

---
+++ Task67/Program.cs
@@ -0,0 +1,13 @@
+﻿// Пр-ма, которая будет принимать на вход число
+// и возвращать сумму его цифр.
+
+int SumDigits(int num)
+{
+    if (num == 0) return 0;
+    else return num % 10 + SumDigits(num / 10);
+}
+
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumDigits(number));
+
