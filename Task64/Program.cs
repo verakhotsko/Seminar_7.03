87d95fc286cc33b5ac00abc2b4a221a65fb208da
Vera <v.khotsko@gmail.com>
2023-04-11 12:10:50 +0300

    Рекурсия от большего числа к меньшему.

---
+++ Task64/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void NaturalNumbers(int num)
+{
+    if(num == 1) 
+    {
+        Console.Write($"{num}");
+        return;
+    }
+    Console.Write($"{num}, ");
+    NaturalNumbers(num -1);
+    
+}
+
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NaturalNumbers(number);
+
