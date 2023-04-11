46619f76d004d7db8fdd664c0c55ea6c398e9192
Vera <v.khotsko@gmail.com>
2023-04-11 14:12:18 +0300

    Рекурсия суммы чисел в заданном промежутке

---
+++ Task66/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+
+bool CheckNaturalNumber(int num)
+{
+    return num >= 1; //Найменьшее натуральное число = 1
+}
+
+int SumNumbers(int num1, int num2)
+{
+    if(num1 == num2) return num2;
+    {
+        if(num1 < num2)
+        return num1 + SumNumbers(num1 + 1, num2);
+        else
+        return num1 + SumNumbers(num1 - 1, num2);
+    }
+}
+
+Console.WriteLine("Введите натуральное значение M");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное значение N");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumbers(number1, number2);
+
+if(CheckNaturalNumber(number1) && CheckNaturalNumber(number2))
+{
+    Console.WriteLine($"M = {number1}; N = {number2} -> {sumNumbers}");
+}
+else 
+{
+    Console.WriteLine("Введенные вами числа - не натуральные");
+}
+
