52a1eba9da573b0a38c889a150e5a50a30f75c0f
Vera <v.khotsko@gmail.com>
2023-04-11 12:14:15 +0300

    Рекурсия по выводу чисел от 1 до заданного.

---
+++ Task63/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значение N.
+// Пр-ма, которая выведет все натуральные числа в 
+// промежутке от 1 до N.
+
+// метод-рекурсия, выводящий числа в заданном промежутке:
+
+void NaturalNums(int num)
+{
+    if(num == 0) return;
+    NaturalNums (num - 1);
+    Console.Write($"{num} ");
+}
+
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NaturalNums(number);
