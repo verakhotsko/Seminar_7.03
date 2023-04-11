726830ef09cf1d7d88630642bcbdd8a62bb48754
Vera <v.khotsko@gmail.com>
2023-04-11 12:13:17 +0300

    Рекурсия по выводу чисел в заданном промежутке

---
+++ Task65/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значения M and N.
+// Пр-ма, которая выведет все натуральные числа в
+// промежутке от М до N.
+
+void NaturalNumsRange(int num1, int num2)
+{
+  if (num1 < num2) 
+  {
+    Console.Write($"{num1} ");
+    NaturalNumsRange(num1 + 1, num2);
+  }
+  else if (num1 > num2)
+  {
+    Console.Write($"{num1} ");
+    NaturalNumsRange(num1-1, num2);
+  }
+  else 
+  {
+    Console.WriteLine(num1);
+  }
+}
+
+Console.Write("Введите первое число диапазона: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите последнее число диапазона: ");
+int n = Convert.ToInt32(Console.ReadLine());
+NaturalNumsRange(m, n);
